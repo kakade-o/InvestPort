fe96ce47b522ec5d49c985c14ba2ecedb462de15
oakakade <omkar710@gmail.com>
2020-06-10 17:36:40 -0400

    Added Login,Logout,Register and Profile page

--- investport/urls.py
+++ investport/urls.py
@@ -7,7 +7,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('register', views.register, name='register'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='investport/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='investport/logout.html'), name='logout'),
     # path('profile', views.profile, name='profile'),
     # path('login/', auth_views.LoginView.as_view(template_name='investport/login.html'), name='login'),
     # path('logout/', auth_views.LogoutView.as_view(template_name='investport/logout.html'), name='logout'),
--- investport/views.py
+++ investport/views.py
@@ -28,39 +28,57 @@ def home(request):
 #     return render(request, 'signup.html', {'form': form})
 
 
+# def register(request):
+#     if request.method == 'POST':
+#         form = forms.SignUpForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             user.refresh_from_db()  # load the profile instance created by the signal
+#             user.profile.birth_date = form.cleaned_data.get('birth_date')
+#             user.save()
+#             raw_password = form.cleaned_data.get('password1')
+#             user = authenticate(username=user.username, password=raw_password)
+#             login(request, user)
+#             return redirect('')
+#     else:
+#         form = forms.SignUpForm()
+#     return render(request, 'investport/register.html', {'form': form})
+
 def register(request):
     if request.method == 'POST':
+        # form = UserCreationForm(request.POST) # POST => form with the users input
         form = forms.SignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            user.refresh_from_db()  # load the profile instance created by the signal
-            user.profile.birth_date = form.cleaned_data.get('birth_date')
-            user.save()
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=user.username, password=raw_password)
-            login(request, user)
-            return redirect('')
+            form.save() # saves to database
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You can now log in.')
+            return redirect('login')
     else:
+        # form = UserCreationForm()
         form = forms.SignUpForm()
     return render(request, 'investport/register.html', {'form': form})
 
-#
-# @login_required
-# def profile(request):
-#     if request.method == 'POST':
-#         u_form = forms.UserUpdateForm(request.POST, instance=request.user)
-#         p_form = forms.ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-#         if u_form.is_valid() and p_form.is_valid():
-#             u_form.save()
-#             p_form.save()
-#             messages.success(request, f'Your account has been updated!')
-#             return redirect('profile')
-#     else:
-#         u_form = forms.UserUpdateForm(instance=request.user)
-#         p_form = forms.ProfileUpdateForm(instance=request.user.profile)
-#     context = {
-#         'u_form': u_form,
-#         'p_form': p_form
-#     }
-#
-#     return render(request, 'investport/profile.html', context)
+@login_required
+def profile(request):
+    return render(request, 'investport/profile.html')
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = forms.UserUpdateForm(request.POST, instance=request.user)
+        p_form = forms.ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+    else:
+        u_form = forms.UserUpdateForm(instance=request.user)
+        p_form = forms.ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'investport/profile.html', context)
